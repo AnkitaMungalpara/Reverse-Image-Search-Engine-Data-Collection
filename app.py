4a24d2e1603c6646e9784ae6606f0e273608fbed
AnkitaMungalpara <mungalpara.ankita@gmail.com>
2024-09-03 21:09:08 -0400

    app.py update

--- app.py
+++ app.py
@@ -159,4 +159,6 @@ def bulk_upload(label, files: List[UploadFile] = File(...)):
 
 
 if __name__ == "__main__":
+    print("starting server")
     uvicorn.run(app, host="0.0.0.0", port=8080)
+    print("server started")
